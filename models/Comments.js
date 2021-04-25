c32e5e24fe3662f21a6c408cff9e18b1a3b99968
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-24 21:31:23 -0700

    Comments Schema set up

---
+++ models/Comments.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const commentsSchema = new mongoose.Schema(
+    {
+        body: {type: String, required: true, min: 10, max: 140}
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Comments = mongoose.model("Comments", commentsSchema);
+
+module.exports = Comments;
