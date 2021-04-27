dca644c771594d2bbf330c05aeda9fc1ad78b6a4
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-27 11:29:25 -0700

    Recipes show route up and running

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 const db = require("../models");
 
-
+// Show Route
+router.get("/showRecipes/:index", function (request, response) {
+    const context = {
+      Recipes: db.Recipes[request.params.index],
+      index: request.params.index,
+    };
+    response.render("recipes/showRecipes", context);
+    // response.send(context);
+  });
 
 module.exports = router;
