69ea8bd6e8a7c8c2d34efc53688c78c0ab9e8c3f
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-29 15:26:50 -0700

    Search bar work

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,6 +4,30 @@ const router = express.Router();
 
 const db = require("../models");
 
+// Index Route
+router.get("/recipes/indexRecipes", async function (req, res) {
+	
+  try {
+    const query = req.query.name
+      ? {
+          name: { $regex: req.query.name, $options: "i" },
+          user: req.session.currentUser.id,
+        }
+      : {
+          user: req.session.currentUser.id,
+        };
+    // const query = {name: { $regex: req.query.name, $options: "i"} };
+
+    const allRecipes = await db.Recipes.find(query);
+    const context = { Recipes: allRecipes };
+
+    return res.render("/recipes/indexRecipes/:id", context);
+  } catch (err) {
+    console.log(err);
+    return res.send(err);
+  }
+});
+
 // Show Route
 router.get("/showRecipes/:id", function (req, res) {
   db.Recipes.findById(req.params.id)
