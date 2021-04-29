0cc97579f5897729c011c452e4cf275d9478f105
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-29 16:34:31 -0700

    Search is done

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,39 +4,32 @@ const router = express.Router();
 
 const db = require("../models");
 
-// Index Route
-router.get("/recipes/indexRecipes/:id", async function (req, res) {
-	
-  try {
-    const query = req.query.name
-      ? {
-          name: { $regex: req.query.name, $options: "i" },
-          user: req.session.currentUser.id,
-        }
-      : {
-          user: req.session.currentUser.id,
-        };
-    // const query = {name: { $regex: req.query.name, $options: "i"} };
-
-    const allRecipes = await db.Recipes.find(query);
-    const context = { Recipes: allRecipes };
-
-    return res.render("/recipes/indexRecipes/:id", context);
-  } catch (err) {
-    console.log(err);
-    return res.send(err);
-  }
-});
 
 // Show Route
 router.get("/showRecipes/:id", function (req, res) {
   db.Recipes.findById(req.params.id)
-    .populate("comments")
-    .exec(function(err, foundRecipes){
-      if (err) return res.send(err);
-
-      const context = {Recipes: foundRecipes};
+  .populate("comments")
+  .exec(function(err, foundRecipes){
+    if (err) return res.send(err);
+    
+    const context = {Recipes: foundRecipes};
     res.render("recipes/showRecipes", context);
+  });
+});
+
+// Index Route
+router.get("/indexRecipes", function (req, res){
+  const query = req.query.name
+      ? {
+          name: { $regex: req.query.name, $options: "i" },
+        }
+      : {
+        };
+  db.Recipes.find(query, function (err,  foundRecipe){
+    if (err) return res.send(err);
+
+    const context = { allRecipes: foundRecipe };
+    res.render("recipes/indexRecipes", context);
   });
 });
 
